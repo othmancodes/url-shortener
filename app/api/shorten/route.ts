774ce080d6aeb8b79f325fc0260ce15ef76eaa57
othmancodes <info.othmanbenyahyaa@gmail.com>
2024-12-30 13:02:00 +0100

    debugging code

--- app/api/shorten/route.ts
+++ app/api/shorten/route.ts
@@ -1,15 +1,18 @@
 import prisma from "@/lib/db";
 import { nanoid } from "nanoid";
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request:NextRequest) {
-    const {url} = await request.json();
-    const shortCode=nanoid(8);
-    const shortenedUrl=await prisma.url.create({
-        data:{
-            originalUrl:url,
-            shortCode
-        }
-    })
-    return NextResponse.json({shortCode:shortenedUrl.shortCode })
+export async function POST(request: NextRequest) {
+  const { url } = await request.json();
+  const shortCode = nanoid(8); // Generate a unique shortcode (8 characters)
+  
+  // Save the URL and shortcode to the database using Prisma
+  const shortenedUrl = await prisma.url.create({
+    data: {
+      originalUrl: url,
+      shortCode
+    }
+  });
+
+  return NextResponse.json({ shortCode: shortenedUrl.shortCode });
 }
